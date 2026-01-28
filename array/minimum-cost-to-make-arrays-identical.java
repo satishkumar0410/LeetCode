f2866002be8ec83c26acfd83b979fca4fca9a76f
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-01-28 14:08:35 +0530

    LC-minimum-cost-to-make-arrays-identical [Runtime: N/A; Memory: N/A]

--- array/minimum-cost-to-make-arrays-identical.java
+++ array/minimum-cost-to-make-arrays-identical.java
@@ -1,7 +1,7 @@
 class Solution {
     public long minCost(int[] arr, int[] brr, long k) {
         int res2 = 0;
-    for(int i=0; i<Math.max(arr.length,brr.length);i++){
+        for(int i=0; i<Math.max(arr.length,brr.length);i++){
             res2+= Math.abs(arr[i]-brr[i]);
         }
         Arrays.sort(arr);
@@ -10,11 +10,6 @@ class Solution {
         for(int i = 0; i < Math.max(arr.length,brr.length);i++){
         if(arr[i] == brr[i]){
             return 0;
-            }
-        }
-        for(int i = 0; i < Math.max(arr.length,brr.length);i++){
-        if(arr[i] == -brr[i]){
-            return Math.abs(2*arr[i]);
             }
         }
         for(int i=0; i<Math.max(arr.length,brr.length);i++){
