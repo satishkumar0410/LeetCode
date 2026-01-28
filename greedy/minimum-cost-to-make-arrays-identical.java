657be9035e26f0c0d2cd1cd3a1a0169e3bdfef10
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-01-28 14:11:07 +0530

    LC-minimum-cost-to-make-arrays-identical [Runtime: N/A; Memory: N/A]

--- greedy/minimum-cost-to-make-arrays-identical.java
+++ greedy/minimum-cost-to-make-arrays-identical.java
@@ -7,11 +7,6 @@ class Solution {
         Arrays.sort(arr);
         Arrays.sort(brr);
         int res = 0;
-        for(int i = 0; i < Math.max(arr.length,brr.length);i++){
-        if(arr[i] == brr[i]){
-            return 0;
-            }
-        }
         for(int i=0; i<Math.max(arr.length,brr.length);i++){
             res+= Math.abs(arr[i]-brr[i]);
         }
