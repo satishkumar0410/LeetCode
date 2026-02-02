62af2266025cf9dc3f3c265aa0b0db17df173115
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-02-02 11:37:10 +0530

    LC-find-smallest-letter-greater-than-target [Runtime: 0 ms; Memory: 46.4 MB]

--- array/find-smallest-letter-greater-than-target.java
+++ array/find-smallest-letter-greater-than-target.java
@@ -1,6 +1,5 @@
 class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
-        Arrays.sort(letters);
         for(int i = 0; i < letters.length; i++){
             if(letters[i]>target){
                 return letters[i];
