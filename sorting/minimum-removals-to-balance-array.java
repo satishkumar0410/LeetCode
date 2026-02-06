6eb56123d08e8429bbdecd3f82bdd1a4b3136587
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-02-06 22:40:37 +0530

    LC-minimum-removals-to-balance-array [Runtime: N/A; Memory: N/A]

---
+++ sorting/minimum-removals-to-balance-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        int j = 0;
+        int removecount = 0;
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            while(nums[j]*k < nums[i]){
+                j++;
+            }
+            removecount = Math.max(removecount,j-i+1);
+        }
+        return nums.length-1-removecount;
+    }
+}
