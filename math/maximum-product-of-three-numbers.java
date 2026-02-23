471da266df671a7ee13b29378f60cc8a4e2f942f
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-02-23 14:08:13 +0530

    LC-maximum-product-of-three-numbers [Runtime: 16 ms; Memory: 48.3 MB]

--- math/maximum-product-of-three-numbers.java
+++ math/maximum-product-of-three-numbers.java
@@ -2,6 +2,7 @@ class Solution {
     public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        return Math.max(nums[0]*nums[1]*nums[2],nums[n-1]*nums[n-2]*nums[n-3]);
+        int max = 0;
+        return Math.max(nums[0]*nums[1]*nums[n-1],nums[n-1]*nums[n-2]*nums[n-3]);
     }
 }
