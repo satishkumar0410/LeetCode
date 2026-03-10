96ecb117dbdf6472e58cd60eb74634155d6e65cd
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-03-10 20:16:18 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

---
+++ hash-table/longest-consecutive-sequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> map = new HashSet<>();
+        for (int n : nums) {
+            map.add(n);
+        }
+        int count = 0;
+        for (int n : numSet) {
+            if (!map.contains(n - 1)) {
+                int l = 1;
+                while (map.contains(n + length)) {
+                    l++;
+                }
+                count = Math.max(count, l);
+            }
+        }
+        return count;
+    }
+}
