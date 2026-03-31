9ac2bd57eb48cb947149e6c69efd190b57b3121b
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-03-31 23:33:47 +0530

    LC-can-place-flowers [Runtime: 1 ms; Memory: 47.6 MB]

--- greedy/can-place-flowers.java
+++ greedy/can-place-flowers.java
@@ -2,7 +2,9 @@ class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
         int x = flowerbed.length;
         for(int i = 0; i < x; i++){
-            if(boolean atu = i == 0 || flowerbed[i-1] == 0; &&  boolean itu = i == flowerbed.length-1 || flowerbed[i+1] == 0; && flowerbed[i] == 0){
+            boolean atu;
+            boolean itu;
+            if((atu = i == 0 || flowerbed[i-1] == 0) && (itu = i == flowerbed.length-1 || flowerbed[i+1] == 0) && flowerbed[i] == 0){
                 flowerbed[i] = 1;
                 n--;
             }
