02894c916c4882b88e36fa9a1ef23fa2e5f0b766
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-04-07 00:28:35 +0530

    LC-container-with-most-water [Runtime: 5 ms; Memory: 77.2 MB]

--- array/container-with-most-water.java
+++ array/container-with-most-water.java
@@ -3,9 +3,7 @@ class Solution {
         int maxArea = 0;
         int left = 0; 
         int right = height.length - 1; 
-
         while (left < right) {
-            
             int width = right - left;
             int currentHeight = Math.min(height[left], height[right]);
             int currentArea = currentHeight * width;
