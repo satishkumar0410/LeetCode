6df5169e294c6ba82d2c786bc743539716e07c0d
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-04-13 11:29:33 +0530

    LC-sort-colors [Runtime: 0 ms; Memory: 43.7 MB]

--- array/sort-colors.java
+++ array/sort-colors.java
@@ -1,5 +1,28 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int low = 0;
+        int mid = 0;
+        int n = nums.length;
+        int high = n-1;
+
+        while (mid <= high) {
+            if(nums[mid] == 0){
+                swap(nums,low,mid);
+                low++;
+                mid++;
+            }
+            else if ( nums [mid] == 1){
+                mid++;
+            }
+            else {
+                swap(nums,mid,high);
+                high--;
+            }
+        }
+    }
+    private void swap(int[]nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums [j] = temp;
     }
 }
