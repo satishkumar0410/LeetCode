6248df20f0d0720ca8579807efd32af54dde3666
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-04-27 11:35:23 +0530

    LC-kids-with-the-greatest-number-of-candies [Runtime: 1 ms; Memory: 43.6 MB]

---
+++ array/kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ls = new ArrayList<>();
+        int max = 0;
+        for(int candie : candies){
+            if(candie > max){
+                max = candie;
+            }
+        }
+        for(int i = 0; i < candies.length;i++){
+            if(candies[i]+extraCandies >= max){
+                ls.add(true);
+            }
+            else {
+                ls.add(false);
+            }
+        }
+        return ls;
+    }
+}
