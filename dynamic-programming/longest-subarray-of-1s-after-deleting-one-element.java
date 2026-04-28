61174f370c15380bc3c9044da7bedcdc66f51473
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-04-28 23:08:24 +0530

    LC-longest-subarray-of-1s-after-deleting-one-element [Runtime: N/A; Memory: N/A]

--- dynamic-programming/longest-subarray-of-1s-after-deleting-one-element.java
+++ dynamic-programming/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,9 +1,15 @@
 class Solution {
        public int longestSubarray(int[] A) {
-        int i = 0, j, k = 1;
+        int i = 0,
+        int j = 0;
+        int k = 1;
         for (j = 0; j < A.length; ++j) {
-            if (A[j] == 0) k--;
-            if (k < 0 && A[i++] == 0) k++;
+            if (A[j] == 0) {
+                k--;
+            }
+            if (k < 0 && A[i++] == 0){
+                 k++;
+            }
         }
         return j - i - 1;
     }
