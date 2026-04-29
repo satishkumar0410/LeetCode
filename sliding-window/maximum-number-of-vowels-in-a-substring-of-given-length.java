10d08a0d8a0bfdb514b316def42e1126d9788035
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-04-29 11:53:08 +0530

    LC-maximum-number-of-vowels-in-a-substring-of-given-length [Runtime: N/A; Memory: N/A]

--- sliding-window/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ sliding-window/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,21 +1,22 @@
+java
 class Solution {
     public int maxVowels(String s, int k) {
-        int [] chars = new int[26];
+        int[] chars = new int[128];
         chars['a']++;
         chars['e']++;
         chars['i']++;
         chars['o']++;
         chars['u']++;
-        char[] puka =s.toCharArray();
-        int res = 0;
-        for(int i = 0; i<k;i++){
-            res += chars[puka[i]];
+        char[] arr = s.toCharArray();
+        int current = 0;
+        for (int i = 0; i < k; i++) {
+            current += chars[arr[i]];
         }
-        int max = res;
-        for(int i = 0, j = k; j < puka.length; i++, j++){
-            res -= chars[puka[j]];
-            res += chars[puka[j]];
-            max = Math.max(max, res);
+        int max = current;
+        for (int i = 0, j = k; j < arr.length; i++, j++) {
+            current -= chars[arr[i]];
+            current += chars[arr[j]];
+            max = Math.max(max, current);
         }
         return max;
     }
