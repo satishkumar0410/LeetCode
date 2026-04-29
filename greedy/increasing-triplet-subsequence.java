5baa36d694fed1749532c18f132a5ab79af07a84
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-04-29 15:15:19 +0530

    LC-increasing-triplet-subsequence [Runtime: 6 ms; Memory: 127 MB]

--- greedy/increasing-triplet-subsequence.java
+++ greedy/increasing-triplet-subsequence.java
@@ -1,15 +1,23 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-            int n = nums.length;
-            for(int i = 0; i < n ; i++){
-                for(int j = i+1; j < n; j++){
-                    for(int k = j+1; k<n; k++){
-                        if(nums[i] < nums[j] && nums[j] < nums[k]){
-                            return true;
-                        }
-                    }
-                }
+        int n = nums.length;
+        if (n < 3) return false;
+        int[] minLeft = new int[n];
+        minLeft[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            minLeft[i] = Math.min(minLeft[i - 1], nums[i]);
+        }
+
+        int[] maxRight = new int[n];
+        maxRight[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            maxRight[i] = Math.max(maxRight[i + 1], nums[i]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > minLeft[i] && nums[i] < maxRight[i]) {
+                return true;
             }
-            return false;
+        }
+        return false;
     }
 }
