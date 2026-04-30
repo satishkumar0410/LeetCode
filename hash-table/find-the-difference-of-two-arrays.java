21cfd50b613d0cd1e7615abc758f9fb1e5e9126c
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-04-30 12:28:18 +0530

    LC-find-the-difference-of-two-arrays [Runtime: 10 ms; Memory: 47.6 MB]

---
+++ hash-table/find-the-difference-of-two-arrays.java
@@ -0,0 +1,14 @@
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>());
+        ans.add(new ArrayList<>());
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        for (int i : nums1) set1.add(i);
+        for (int i : nums2) set2.add(i);
+        for(int i : set1) if(!set2.contains(i)) ans.get(0).add(i);
+        for(int i : set2) if(!set1.contains(i)) ans.get(1).add(i);
+        return ans;
+    }
+}
