88a94163aa4028ee41e3e789ab366446948ff569
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-05-01 15:45:41 +0530

    LC-decode-string [Runtime: 0 ms; Memory: 43 MB]

--- stack/decode-string.java
+++ stack/decode-string.java
@@ -1,24 +1,30 @@
 class Solution {
     public String decodeString(String s) {
-        Deque<Character> queue = new LinkedList<>();
-        for (char c : s.toCharArray()) queue.offer(c);
-        return helper(queue);
+        Deque<Character> laudesh = new ArrayDeque<>();
+        for(char c : s.toCharArray()) {
+            laudesh.add(c);    
+        }
+        return pukpulihora(laudesh);
     }
-    
-    public String helper(Deque<Character> queue) {
+    public String pukpulihora(Deque<Character> laudesh) {
         StringBuilder sb = new StringBuilder();
-        int num = 0;
-        while (!queue.isEmpty()) {
-            char c= queue.poll();
-            if (Character.isDigit(c)) {
-                num = num * 10 + c - '0';
-            } else if (c == '[') {
-                String sub = helper(queue);
-                for (int i = 0; i < num; i++) sb.append(sub);   
-                num = 0;
-            } else if (c == ']') {
+        int sankya = 0;
+        while(!laudesh.isEmpty()) {
+            char c = laudesh.poll();
+            if(Character.isDigit(c)) {
+                sankya = sankya*10 + (c - '0');
+            }
+            else if (c == '['){
+                String pukesh = pukpulihora(laudesh);
+                for(int i = 0; i < sankya; i++) {
+                    sb.append(pukesh);
+                }
+                sankya = 0;
+            }
+            else if(c == ']') {
                 break;
-            } else {
+            }
+            else {
                 sb.append(c);
             }
         }
