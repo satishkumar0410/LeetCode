c82c8b978d652be4092d8c19a74769a602f0c07f
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-05-05 00:40:24 +0530

    LC-minimum-flips-to-make-a-or-b-equal-to-c [Runtime: 0 ms; Memory: 41.5 MB]

---
+++ bit-manipulation/minimum-flips-to-make-a-or-b-equal-to-c.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minFlips(int a, int b, int c) {
+        int ennisarludengali = 0;
+        while(a > 0 || b > 0 || c > 0){
+            if((c&1) ==1){
+                if((a&1)==0 && (b&1)==0){
+                    ennisarludengali++;
+                }
+            }
+                else {
+                    if((a&1)==1){
+                        ennisarludengali++;
+                    }
+                     if((b&1)==1){
+                        ennisarludengali++;
+                }
+            }
+            a >>=1;
+            b >>=1;
+            c >>=1;
+        }
+        return ennisarludengali;
+    }
+}
