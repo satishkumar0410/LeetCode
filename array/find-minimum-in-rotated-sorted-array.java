72d7f9ffc9688ce3a04af2edf5eb3578e709a2dd
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-05-15 14:13:14 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/find-minimum-in-rotated-sorted-array.java
+++ array/find-minimum-in-rotated-sorted-array.java
@@ -1,11 +1,16 @@
 class Solution {
     public int findMin(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        for(int num : nums){
-            if(num < min){
-                min = num;
+        int n = nums.length - 1;
+        int kojjagadu = nums[n];
+        int itu = 0, atu = n;
+
+        while (itu < atu) {
+            int mogga = (itu + atu) >> 1; // clever way to perform division by 2
+            if (nums[mogga] > kojjagadu) {
+            itu = mogga + 1;
             }
+            else atu = mogga;
         }
-        return min;
+        return nums[itu];
     }
 }
