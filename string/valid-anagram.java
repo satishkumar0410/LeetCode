867b22fcbe2c52a21f75e16c3a316ce4cad03ec9
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-05-22 16:31:26 +0530

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- string/valid-anagram.java
+++ string/valid-anagram.java
@@ -1,19 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        for(char c : s.toCharArray()){
-            map.put(c, map.getOrDefault(c, 0) + 1);
-        }
-        
-        for(char i : t.toCharArray()){
-            if(map.containsKey(i) && map.get(i) > 0){
-                map.put(i, map.get(i) - 1);
-            } else {
-                return false;
-            }
-        }
-        return true;
+        char [] sarr = s.toCharArray();
+        char [] tarr = t.toCharArray();
+        Arrays.sort(sarr);
+        Arrays.sort(tarr);
+        if(s.length()!=t.length()) return false;
+        return Arrays.equals(sarr,tarr);
     }
 }
