14d1a9e3b08c7e38ebd2fa0f9d05f932c5f1229e
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-06-19 15:40:19 +0530

    LC-fibonacci-number [Runtime: N/A; Memory: N/A]

--- memoization/fibonacci-number.java
+++ memoization/fibonacci-number.java
@@ -1,14 +1,11 @@
 class Solution {
-    int [] memo = new int[100];
     public int fib(int n) {
-        Arrays.fill(memo,-1);
-        return solve(n);
-    }
-    private int solve(int n){
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(memo[n] != -1) return memo[n];
-        memo[n] = solve(n-1)+solve(n-2);
-        return memo[n];
+        int[] dp = new int [100];
+        dp[0] = 0; 
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
