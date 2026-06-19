20c6ae6b2482c1273d2d8eb506682ff71e9f544b
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-06-19 15:27:37 +0530

    LC-fibonacci-number [Runtime: N/A; Memory: N/A]

--- dynamic-programming/fibonacci-number.java
+++ dynamic-programming/fibonacci-number.java
@@ -1,12 +1,14 @@
 class Solution {
+    int [] memo = new int[100];
     public int fib(int n) {
-        int [] memo = new int [100];
         Arrays.fill(memo,-1);
+        return solve(n);
+    }
+    private int solve(int n){
         if(n==0) return 0;
         if(n==1) return 1;
-        if(n==2) return 1;
         if(memo[n] != -1) return memo[n];
-        memo[n] = fib(n-2)+ fib(n-1);
+        memo[n] = solve(n-1)+solve(n-2);
         return memo[n];
     }
 }
