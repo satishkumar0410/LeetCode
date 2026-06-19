00493c2f9df1eab53fef5eb0a11e85a1df7003da
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-06-19 15:46:17 +0530

    LC-fibonacci-number [Runtime: N/A; Memory: N/A]

--- math/fibonacci-number.java
+++ math/fibonacci-number.java
@@ -1,11 +1,15 @@
 class Solution {
     public int fib(int n) {
-        int[] dp = new int [100];
-        dp[0] = 0; 
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int prev2 = 0;
+        int prev1 = 1;
+        int curr = 0;
+        for (int i = 2; i <= n; i++) {
+            curr  = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[n];
+        return curr;
     }
 }
