0381ca8cd8580c9f88aa6e05b310dfe61eff5d68
Satish Kumar Vamisetti <satishvamisetti04@gmail.com>
2026-06-21 00:36:20 +0530

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.java
+++ dynamic-programming/house-robber.java
@@ -1,5 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
          int [] dp = new int [100];
          dp[0] = nums[0];
          dp[1] = Math.max(nums[0],nums[1]);
